9092de6aa3339c2793dcb02bb03159478e5acaaa
Yuki Kanayama <kanayama@spicato.com>
2025-02-19 19:33:29 +0900

    sharpの組み込み

--- bin/gyuto.js
+++ bin/gyuto.js
@@ -1,2 +1,33 @@
 #!/usr/bin/env node
-console.log("Hello world");
+const { spawn } = require("child_process");
+const command = process.argv[2];
+const target = process.argv[3];
+
+if (!command || !target) {
+  console.log("Usage: gyuto [all|watch] <directory>");
+  process.exit(1);
+}
+
+switch (command) {
+  case "all":
+    spawn("node", [`${__dirname}/../sharp/sharp-all.mjs`, target], {
+      stdio: "inherit",
+    });
+    break;
+  case "watch":
+    spawn(
+      "onchange",
+      [
+        target,
+        "--",
+        "node",
+        `${__dirname}/../sharp/sharp-watch.mjs`,
+        "{changed}",
+      ],
+      { stdio: "inherit" }
+    );
+    break;
+  default:
+    console.log("Usage: gyuto [all|watch] <directory>");
+    break;
+}
